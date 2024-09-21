4977aa73ee77193711db5d018d8bace6567326fb
Maxime PIGEON <maxpige@gmail.com>
2024-09-21 17:07:09 +0200

    fix link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  redirect("https://info.lalarme.com");
+  redirect("https://info.lalarme.com/fr/landing");
 
   return <div>La Larme</div>;
 }
